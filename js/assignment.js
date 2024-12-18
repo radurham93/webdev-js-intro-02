9567ea632e6266c3e818d7b252fd819e793562b1
Robert Durham <radurham93@gmail.com>
2024-12-17 19:31:04 -0500

    changed javascript

--- js/assignment.js
+++ js/assignment.js
@@ -2,29 +2,42 @@
 
 // Declaring constant variables to store references to the elements
 // that will be updated by your code.
-const yearElement = document.getElementById("year");
+const yearElement = document.getElementById("year")
 const resolutionElement = document.getElementById("resolution")
 const willMeetResolutionElement = document.getElementById("will-meet-resolution")
 const submissionBtn = document.getElementById("submission-btn")
 
 // Declare your variables here.
 
+const resolution = "Learn JavaScript";
+const currentYear = "2024";
+let willMeetResolution = true;
+
 function updateYear() {
     // write the logic
-
+yearElement.innerText = currentYear;
 }
 
 function updateResolution() {
     // Update this function 
-
+resolutionElement.innerText = resolution;
 }
+
+function updateWillMeetResolution() {
+    // Update this function
+willMeetResolutionElement.innerText = willMeetResolution;
+}
+
 
 function render() {
     // Finish writing this function
+    updateResolution();
+    updateYear();
+    updateWillMeetResolution();
 
 }
 
 submissionBtn.addEventListener("click", function () {
     // Finish writing this function
-
+render();
 })
